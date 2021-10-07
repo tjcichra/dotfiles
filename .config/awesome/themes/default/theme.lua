ac76ea9bcea2bed40d4a2b5b508b9806ec3b9aec
tjcichra <8120075+tjcichra@users.noreply.github.com>
2021-10-06 21:40:21 -0400

    fixed awesomewm wallpaper

--- .config/awesome/themes/default/theme.lua
+++ .config/awesome/themes/default/theme.lua
@@ -108,7 +108,8 @@ theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/m
 
 --theme.wallpaper = themes_path.."default/background.png"
 --theme.wallpaper = "~/.config/awesome/themes/default/wallpaper.jpg"
-theme.wallpaper = themes_path.."default/archwave.png"
+--theme.wallpaper = themes_path.."default/archwave.png"
+theme.wallpaper = "~/.config/awesome/themes/default/archwave.png"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
